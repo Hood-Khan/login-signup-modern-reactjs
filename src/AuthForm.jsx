5b533e1494eda7f0da9439a4d8dbeee39e1736b1
HoodKhan <muhammadhoodkhan9366@gmail.com>
2025-11-21 00:11:53 +0500

    first commit

---
+++ src/AuthForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AuthForm() {
+  return (
+    <div>
+      <button>Login</button>
+      <button>Signup</button>
+    </div>
+  )
+}
+
+export default AuthForm
